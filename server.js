a456c3150889e6645d1e04e454721913fb010a2d
gokul-raj-2 <gokul8262@gmail.com>
2020-05-10 18:26:53 +0530

    Integrating Backend

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const cors = require('cors');
-const ytdl = require('ytdl-core');
+const youtubedl = require('youtube-dl');
 const app=express();
 
 app.use(cors());
 
 app.get('/download', (req,res) => {
 	var URL = req.query.URL;
-	res.header('Content-Disposition', 'attachment; filename="video.mp4"');
-	ytdl(URL, {
-	    format: 'mp4'
-	    }).pipe(res);
+  youtubedl.getInfo(URL, [], (err, info) => {
+    if (err) res.json(err);
+    res.json(info.url);
+  }); 
 });
 
-app.listen(3000,() => {
 
-});
+app.listen(3000,() => {});
